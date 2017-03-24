5af53af4b2961b46846f6ca33d0863b72869cb2b
Daniel R Gulotta <dgulotta@alum.mit.edu>
2017-03-24 09:28:40 -0400

    add 'balanced' option for walk

--- walk/walk.cpp
+++ walk/walk.cpp
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *   Copyright (C) 2017 by Daniel Gulotta                                  *
+ *   dgulotta@alum.mit.edu                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA         *
+ *   02110-1301  USA                                                       *
+ ***************************************************************************/
+
 #include <tuple>
 #include "../randgen.h"
 #include "walk.h"
@@ -85,14 +105,32 @@ wrap_canvas<uint8_t> levels(const wrap_canvas<int> &c)
 	return sc;
 }
 
-wrap_canvas<color_t> draw_walk(int width, int height)
+wrap_canvas<color_t> draw_walk(int width, int height, bool balanced)
 {
 	wrap_canvas<int> cr(width,height);
 	wrap_canvas<int> cg(width,height);
 	wrap_canvas<int> cb(width,height);
 	std::vector<double> probs(6);
-	for(int i=0;i<6;i++)
-		probs[i]=random_exponential(1.);
+	if(balanced) {
+		std::vector<double> tprobs(6);
+		do {
+			for(int i=0;i<6;i++)
+				tprobs[i]=random_normal(1.);
+			double p1=(tprobs[0]+tprobs[1]-tprobs[2]-tprobs[3])/4;
+			double p2=(tprobs[0]-tprobs[1]-tprobs[2]
+				+tprobs[3]-2*tprobs[4]+2*tprobs[5])/12;
+			probs[0]=tprobs[0]-p1-p2;
+			probs[1]=tprobs[1]-p1+p2;
+			probs[2]=tprobs[2]+p1+p2;
+			probs[3]=tprobs[3]+p1-p2;
+			probs[4]=tprobs[4]+2*p2;
+			probs[5]=tprobs[5]-2*p2;
+		} while(any_of(probs.begin(),probs.end(),[] (double x) { return x<0; }));
+	}
+	else {
+		for(int i=0;i<6;i++)
+			probs[i]=fabs(random_normal(1.));
+	}
 	for(int i=0;i<width;i++)
 		for(int j=0;j<height;j++) {
 			switch(random_weighted(probs)) {
--- walk/walkwidget.cpp
+++ walk/walkwidget.cpp
@@ -1,6 +1,7 @@
 #include "../inputwidgets.h"
 #include "walk.h"
 #include "walkwidget.h"
+#include <QCheckBox>
 
 WalkWidget::WalkWidget()
 {
@@ -8,7 +9,10 @@ WalkWidget::WalkWidget()
 	layout()->addRow("Height",ht);
 	auto wd = new SizeSpin(1);
 	layout()->addRow("Width",wd);
+	auto ba = new QCheckBox("Balanced");
+	ba->setChecked(true);
+	layout()->addRow(ba);
 	addGenerator("Draw",[=] () {
-		return draw_walk(wd->value(),ht->value());
+		return draw_walk(wd->value(),ht->value(),ba->isChecked());
 	});
 }
